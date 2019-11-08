d66cf66a13979ad3abb908d5bafb0e2ae5d8ae0e
kinghuin <kinghuin_chull@163.com>
2019-11-08 18:10:55 +0800

    begin autodl

--- demo/text-classification/run_autodl.sh
+++ demo/text-classification/run_autodl.sh
@@ -1,4 +1,4 @@
-dataset=xnli_zh
+dataset_name=xnli_zh
 
 hub autofinetune text_classifier.py --param_file=autodl.yaml --cuda=['2','3','4'] --popsize=3 --round=10 \
- --output_dir=./${dataset}_autodl_output --evaluate_choice=fulltrail --tuning_strategy=hazero
+ --output_dir=./${dataset_name}_autodl_output --evaluate_choice=fulltrail --tuning_strategy=hazero dataset dataset_name

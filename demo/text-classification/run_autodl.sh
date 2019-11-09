b1ca4d78564e9a80188b381700ef92707c257b08
kinghuin <kinghuin_chull@163.com>
2019-11-09 12:40:43 +0800

    debug autodl

--- demo/text-classification/run_autodl.sh
+++ demo/text-classification/run_autodl.sh
@@ -1,4 +1,4 @@
 dataset_name=xnli_zh
 
 python ../../paddlehub/commands/hub.py autofinetune text_classifier.py --param_file=autodl.yaml --gpu=1,2,3,4 --popsize=4 --round=10 \
- --output_dir=./${dataset_name}_autodl_output --evaluator=fulltrail --tuning_strategy=hazero dataset dataset_name
+ --output_dir=./${dataset_name}_autodl_output --evaluator=fulltrail --tuning_strategy=hazero dataset ${dataset_name}

18ffaae0a5b43597314c7bd83def171cd1e13524
kinghuin <kinghuin_chull@163.com>
2019-11-09 12:49:50 +0800

    autodl inews

--- demo/text-classification/run_autodl.sh
+++ demo/text-classification/run_autodl.sh
@@ -1,4 +1,4 @@
-dataset_name=xnli_zh
+dataset_name=inews
 
-python ../../paddlehub/commands/hub.py autofinetune text_classifier.py --param_file=autodl.yaml --gpu=1,2,3,4 --popsize=4 --round=10 \
+python ../../paddlehub/commands/hub.py autofinetune text_classifier.py --param_file=autodl.yaml --gpu=5 --popsize=3 --round=10 \
  --output_dir=./${dataset_name}_autodl_output --evaluator=fulltrail --tuning_strategy=hazero dataset ${dataset_name}

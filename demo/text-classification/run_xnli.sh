80a230fabda939ac4cb4109660835927c4703c3c
kinghuin <kinghuin_chull@163.com>
2019-11-12 10:59:30 +0800

    small autodl

---
+++ demo/text-classification/run_xnli.sh
@@ -0,0 +1,2 @@
+python ../../paddlehub/commands/hub.py autofinetune text_classifier.py --param_file=autodl.yaml --gpu=2,3,4,5 --popsize=12 --round=10 \
+ --output_dir=./autodl.output.xnli.1112 --evaluator=fulltrail --tuning_strategy=hazero dataset xnli

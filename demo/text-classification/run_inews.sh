e645f8872ca6d0e3d5cb7e42fdcb6eec273b52bd
kinghuin <kinghuin_chull@163.com>
2019-11-12 12:03:13 +0800

    begin inews autodl

--- demo/text-classification/run_inews.sh
+++ demo/text-classification/run_inews.sh
@@ -1,2 +1,4 @@
+export FLAGS_eager_delete_tensor_gb=0.0
+
 python ../../paddlehub/commands/hub.py autofinetune text_classifier.py --param_file=autodl.yaml --gpu=2,3,4,5,6,7 --popsize=12 --round=10 \
  --output_dir=./autodl.output.inews.1112 --evaluator=fulltrail --tuning_strategy=hazero dataset inews

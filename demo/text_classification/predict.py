a6c731a7e8bde0229a5a3d7de77efd6c65d3e864
kinghuin <kinghuin_chull@163.com>
2020-03-20 16:08:51 +0800

    accelerate_mode default True

--- demo/text_classification/predict.py
+++ demo/text_classification/predict.py
@@ -39,7 +39,7 @@ args = parser.parse_args()
 
 if __name__ == '__main__':
     # Load Paddlehub ERNIE Tiny pretrained model
-    module = hub.Module(name="ernie_tiny")
+    module = hub.Module(name="roberta_wwm_ext_chinese_L-3_H-768_A-12")
     inputs, outputs, program = module.context(
         trainable=True, max_seq_len=args.max_seq_len)
 
